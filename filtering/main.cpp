d473ebd8c35d0a5415bbe573f0eda67c71c4c664
ldw01441929 <ldw01441929@alibaba-inc.com>
2023-02-13 18:33:19 +0800

    filter tutorial

--- filtering/main.cpp
+++ filtering/main.cpp
@@ -1,7 +1,47 @@
 #include <fmt/core.h>
+#include <pcl/filters/passthrough.h>
+#include <pcl/point_types.h>
 
-int main()
-{
-  fmt::print("Hello, world!\n");
+void random_init_cloud(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud,
+                       decltype(cloud->width) width,
+                       decltype(cloud->height) height) {
+  cloud->width = width;
+  cloud->height = height;
+  cloud->resize(cloud->width * cloud->height);
+
+  for (auto &point : *cloud) {
+    point.x = 1024 * rand() / (RAND_MAX + 1.0F);
+    point.y = 1024 * rand() / (RAND_MAX + 1.0F);
+    point.z = 1024 * rand() / (RAND_MAX + 1.0F);
+  }
+}
+
+void print_cloud(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud) {
+  for (const auto &point : *cloud) {
+    fmt::print("  ({}, {}, {})\n", point.x, point.y, point.z);
+  }
+}
+
+int main() {
+  auto cloud = boost::make_shared<pcl::PointCloud<pcl::PointXYZ>>();
+  auto filtered_cloud = boost::make_shared<pcl::PointCloud<pcl::PointXYZ>>();
+
+  random_init_cloud(cloud, 5, 1);
+
+  fmt::print("Cloud before filtering:\n");
+  print_cloud(cloud);
+
+  pcl::PassThrough<pcl::PointXYZ> pass;
+  pass.setInputCloud(cloud);
+  fmt::print("set filter field name: z\n");
+  pass.setFilterFieldName("z");
+  fmt::print("set filter limits: 0.0 - 1.0\n");
+  pass.setFilterLimits(0.0F, 1.0F);
+  // pass.setNegative(true);
+  pass.filter(*filtered_cloud);
+
+  fmt::print("Cloud after filtering:\n");
+  print_cloud(filtered_cloud);
+
   return 0;
 }
