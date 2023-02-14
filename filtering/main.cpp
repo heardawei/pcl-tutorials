47a499e84e05f02a180d61b3162dee048c1cd89e
李大伟 <ldw01441929@alibaba-inc.com>
2023-02-14 09:40:29 +0800

    filtering

--- filtering/main.cpp
+++ filtering/main.cpp
@@ -4,27 +4,33 @@
 
 void random_init_cloud(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud,
                        decltype(cloud->width) width,
-                       decltype(cloud->height) height) {
+                       decltype(cloud->height) height)
+{
   cloud->width = width;
   cloud->height = height;
   cloud->resize(cloud->width * cloud->height);
 
-  for (auto &point : *cloud) {
+  for (auto &point : *cloud)
+  {
     point.x = 1024 * rand() / (RAND_MAX + 1.0F);
     point.y = 1024 * rand() / (RAND_MAX + 1.0F);
     point.z = 1024 * rand() / (RAND_MAX + 1.0F);
   }
 }
 
-void print_cloud(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud) {
-  for (const auto &point : *cloud) {
+void print_cloud(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud)
+{
+  for (const auto &point : *cloud)
+  {
     fmt::print("  ({}, {}, {})\n", point.x, point.y, point.z);
   }
 }
 
-int main() {
-  auto cloud = boost::make_shared<pcl::PointCloud<pcl::PointXYZ>>();
-  auto filtered_cloud = boost::make_shared<pcl::PointCloud<pcl::PointXYZ>>();
+int main()
+{
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+  pcl::PointCloud<pcl::PointXYZ>::Ptr filtered_cloud(
+      new pcl::PointCloud<pcl::PointXYZ>);
 
   random_init_cloud(cloud, 5, 1);
 
